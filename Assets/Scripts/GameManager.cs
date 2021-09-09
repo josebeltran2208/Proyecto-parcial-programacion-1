215d65e23fbbc07fbec9d14b753d8bbc0eee2bea
josebeltran2208 <josebeltran2208@gmail.com>
2021-09-09 16:24:21 -0500

    Primer parcial

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gamePaused = false;
+    bool gameOver = false;
+    [SerializeField] Spaceship player;
+    [SerializeField] GameObject pauseUI;
+    [SerializeField] GameObject gameOverUI;
+    [SerializeField] int numEnemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseUI.SetActive(false);
+        gameOverUI.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && gameOver == false)
+            PauseGame();
+
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1;
+    }
+
+    void PauseGame()
+    {
+        gamePaused = gamePaused ? false : true;
+
+        player.gamePaused = gamePaused;
+        
+        pauseUI.SetActive(gamePaused);
+
+        Time.timeScale = gamePaused ? 0 : 1;
+    }
+
+    public void ReducirNumEnemigos()
+    {
+        numEnemies = numEnemies - 1;
+        if(numEnemies < 1)
+        {
+            Ganar();
+        }
+    }
+
+    void Ganar()
+    {
+        gameOver = true;
+        Time.timeScale = 0;
+        player.gamePaused = true;
+        gameOverUI.SetActive(true);
+    }
+}
